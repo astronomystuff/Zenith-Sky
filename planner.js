d9531816e584de628d925999ba402808fd42a08d
astronomystuff <niamnbhakta@gmail.com>
2026-03-18 14:11:27 -0700

    Update planner.js

--- planner.js
+++ planner.js
@@ -1497,19 +1497,6 @@ function drawOnScreenMap(lat, lon, dt) {
   drawAzimuthalStarMap(canvas, lat, lon, dt);
 }
 
-const moon = computeMoon(dt);
-const latRad = deg2rad(lat);
-const lonRad = deg2rad(lon);
-const jd = toJulianDate(dt);
-const lst = localSiderealTime(jd, lonRad);
-const raRad = deg2rad(moon.raHours * 15);
-const decRad = deg2rad(moon.decDeg);
-const ha = normalizeAngle(lst - raRad);
-const sinAlt =
-  Math.sin(latRad) * Math.sin(decRad) +
-  Math.cos(latRad) * Math.cos(decRad) * Math.cos(ha);
-moon.altDeg = rad2deg(Math.asin(sinAlt));
-const moonRS = computeRiseSet(lat, moon.decDeg, moon.raHours, dt);
 function renderMapImageForPrint(lat, lon, dt) {
   const inches = 3.5;
   const dpi = 300;
