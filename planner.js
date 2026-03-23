b05da40a21e82120623c34efbdc8c099be5740b1
astronomystuff <niamnbhakta@gmail.com>
2026-03-23 14:22:46 -0700

    Update planner.js

--- planner.js
+++ planner.js
@@ -2182,7 +2182,7 @@ printWin.document.write(`
         table{
           width:100%;
           border-collapse:collapse;
-          font-size:11px;
+          font-size:11.25px;
         }
         th,td{
           border:1px solid #bbb;
