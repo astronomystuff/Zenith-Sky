e5daab0df2f8b93a6459ad7948fb08057dc00bb9
astronomystuff <niamnbhakta@gmail.com>
2026-04-15 20:35:04 -0700

    Update scale calculation for improved responsiveness

--- planner.js
+++ planner.js
@@ -1000,7 +1000,7 @@ const cx = w / 2;
 const cy = h / 2;
 const padding = 40;
 const radius = size / 2 - padding;
-const scale = size / 336;
+const scale = Math.pow(radius / 525, 0.75);
 
   // Background
   ctx.fillStyle = "#ffffff";
