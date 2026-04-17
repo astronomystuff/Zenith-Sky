e90af0876070dc418ee603e1821634e6078ff745
astronomystuff <niamnbhakta@gmail.com>
2026-04-17 11:08:52 -0700

    Adjust scale calculation in planner.js

--- planner.js
+++ planner.js
@@ -1004,7 +1004,7 @@ const cx = w / 2;
 const cy = h / 2;
 const padding = 40;
 const radius = size / 2 - padding;
-const scale = (radius * radius) / 500000 + 0.05;
+const scale = (radius * radius) / 500000 + 0.3;
 
 
   // Background
