22b339b0b3b5d1a510ba888a6ce51aecc8114bbd
astronomystuff <niamnbhakta@gmail.com>
2026-04-17 12:14:13 -0700

    Increase padding from 0 to 10 in planner.js

--- planner.js
+++ planner.js
@@ -1002,7 +1002,7 @@ const w = size;
 const h = size;
 const cx = w / 2;
 const cy = h / 2;
-const padding = 0;
+const padding = 10;
 const radius = size / 2 - padding;
 const scale = (canvas.width * canvas.width) / 500000 + 0.25;
 
