a76c04a8ac330cb67f2ba806a67fa2141067d82d
astronomystuff <niamnbhakta@gmail.com>
2026-04-17 10:48:33 -0700

    Update scale calculation to use radius instead of size

--- planner.js
+++ planner.js
@@ -1004,7 +1004,7 @@ const cx = w / 2;
 const cy = h / 2;
 const padding = 40;
 const radius = size / 2 - padding;
-const scale = (size * size) / 336000 - 0.1;
+const scale = (radius * radius) / 336000 - 0.1;
 
 
   // Background
