7e745e522455516958fac33741e73a40a3ece4f7
astronomystuff <niamnbhakta@gmail.com>
2026-05-20 13:52:06 -0700

    Expose sky3dStarBase as a global variable

--- skyengine.js
+++ skyengine.js
@@ -10,15 +10,14 @@ window.onerror = function (msg, src, line, col, err) {
 let sky3dScene, sky3dCamera, sky3dRenderer;
 let sky3dModalOpen = false;
 let sky3dControls;
-let sky3dStarBase = [];
 let sky3dCelestialSphere = null;
 let sky3dGround = null;
 let sky3dTooltip = null;
 let sky3dRootGroup = null;
+window.sky3dStarBase = [];
 let sky3dRaycaster = new THREE.Raycaster();
   sky3dRaycaster.params.Points.threshold = 0.01;
 let sky3dMouse = new THREE.Vector2();
-
 const J2000_MS = Date.UTC(2000, 0, 1, 12, 0, 0);
 const LY_TO_PC = 1 / 3.26156;
 
