e87c260ca5154cf5701de39ba21d43e74a2577f5
astronomystuff <niamnbhakta@gmail.com>
2026-09-12 21:30:07 -0700

    Reduce maximum hit distance for stars and planets

--- skyengine.js
+++ skyengine.js
@@ -2513,7 +2513,7 @@ function onSky3DClick(event) {
   let bestStar = null;
   let bestStarHit = null;
   let bestStarMag = Infinity;
-  const MAX_HIT_DISTANCE = 0.01;
+  const MAX_HIT_DISTANCE = 0.005;
 
   if (starHits.length > 0) {
     for (const hit of starHits) {
@@ -2535,7 +2535,7 @@ function onSky3DClick(event) {
 
   let bestPlanet = null;
   let bestPlanetHit = null;
-  const MAX_PLANET_DISTANCE = 0.01;
+  const MAX_PLANET_DISTANCE = 0.005;
 
   if (planetHits.length > 0) {
     for (const hit of planetHits) {
